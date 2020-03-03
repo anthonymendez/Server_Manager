aed6ed812bd446095f90848cc1d46ad55b10ae6a
Anthony Mendez <anthonymendez9@gmail.com>
2020-03-02 22:29:00 -0500

    Formatted imports. Specified parameters for regular website run

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
-from flask import render_template
+# General Imports
+import imp
+import os
+
+# Specific Imports
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,10 +14,14 @@ def home_page():
 
 # Run Website w/o Debug Enabled
 def run():
+    app.config['TEMPLATES_AUTO_RELOAD'] = False
+    app.config['DEBUG'] = False
+    app.config['ENV'] = "production"
     app.run()
 
 # Run Website w/ Debug Enabled
 def debug():
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['DEBUG'] = True
+    app.config['ENV'] = "development"
     app.run()
