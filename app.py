4541bd13c0ae5d25f0d1d0d59baa8c3598d515de
Anthony Mendez <anthonymendez9@gmail.com>
2020-03-02 21:52:21 -0500

    Create separate python files & function for prod run and debug run. Finished(?) with plugin example template

--- app.py
+++ app.py
@@ -2,11 +2,18 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Main Page
 @app.route('/')
 def home_page():
     return render_template('index.html')
 
-app.run(debug=True)
+# Run Website w/o Debug Enabled
+def run():
+    app.run()
+
+# Run Website w/ Debug Enabled
+def debug():
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.config['DEBUG'] = True
+    app.run()
