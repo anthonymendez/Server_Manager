99de39a3de425d6efa1b828d4d2e0e7fe37d6007
Anthony Mendez <anthonymendez9@gmail.com>
2020-03-04 23:18:40 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="Server_Manager",
-    version='0.0.1',
+    version='0.0.2',
+    author="Anthony Mendez",
+    author_email="anthonymendez9@gmail.com",
+    description="Server Manager is a Flask web application that allows you create or download plugins to view various statistics for your server.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/anthonymendez/Server_Manager",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'flask',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3.0"
     ],
+    python_requires=">=3.8"
 )
